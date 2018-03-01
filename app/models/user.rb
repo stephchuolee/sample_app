15afcb21e167151b37d16b509ca15c13a2fc5f0a
stephchuolee <schuolee@gmail.com>
2018-03-01 18:20:38 +0000

    Set up tests and validation for User name/email max length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true # same as validates(:name, presence: true)
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 } # same as validates(:name, presence: true)
+  validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,11 +12,22 @@ class UserTest < ActiveSupport::TestCase
   
   test "name should be present" do
     @user.name = "    "
-    assert_not @user.valid?
+    assert_not @user.valid? # meaning this user should be invalid
   end
   
   test "email should be present" do
     @user.email = "   "
     assert_not @user.valid?
   end
+  
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
 end

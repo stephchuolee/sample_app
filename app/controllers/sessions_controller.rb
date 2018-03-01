fdf3c5ccd2014e0f602dcc2b7af82d583c0d0bad
stephchuolee <schuolee@gmail.com>
2018-03-01 23:46:28 +0000

    Set up sessions helper methods and use in sessions successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to user's show page
+      log_in user # log_in(user)
+      redirect_to user
     else 
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination' # flash.now is used because it displays flash messages on rendered pages, not reloads/additional requests

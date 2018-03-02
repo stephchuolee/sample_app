bc5ee6b20bde5c564d81a3a3cf3906556218992e
stephchuolee <schuolee@gmail.com>
2018-03-02 00:19:44 +0000

    Fix typo in users.yml test data, set up test for valid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:steph)
+  end
+  
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -14,5 +18,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  
+  test "login with valid information" do
+    get login_path
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'password'
+      }
+    }
+    assert_redirected_to @user # check the right redirect target
+    follow_redirect! # actually visit the redirect target
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0 # the login link should be gone
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end

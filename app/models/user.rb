f2d4f050c96129c1b286c3cf54477e34e3c2e055
stephchuolee <schuolee@gmail.com>
2018-03-03 01:02:51 +0000

    Set up test data for microposts appearing in desc order. Also set up dependency of microposts in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   before_save { email.downcase! } # same as self.email = email.downcase
   validates :name, presence: true, length: { maximum: 50 } # same as validates(:name, presence: true)

7d148f47046238fbfc1a154f525f3bb1326ec4d1
stephchuolee <schuolee@gmail.com>
2018-03-03 01:03:37 +0000

    Add test for user-micropost dependency

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,12 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  
+  test "associated microposts should be destroyed" do
+    @user.save # so that user gets an id
+    @user.microposts.create!(content: "Lorem")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end

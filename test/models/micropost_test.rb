37bbee9b098cf0ee4509825b9a702982ff335c51
stephchuolee <schuolee@gmail.com>
2018-03-03 00:47:22 +0000

    Add test for micropost models

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:steph)
+    # This code is not idiomatically correct:
+    @micropost = Micropost.new(content: "Lorem", user_id: @user.id)
+  end
+  
+  test "should be valid" do
+    # Sanity check
+    assert @micropost.valid?
+  end
+  
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end

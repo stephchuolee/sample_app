5d817a80252f779b99c85eddb8bf147ec90c7585
stephchuolee <schuolee@gmail.com>
2018-03-04 07:23:06 +0000

    Set up cloud storage for images in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,6 +3,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
+  
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
